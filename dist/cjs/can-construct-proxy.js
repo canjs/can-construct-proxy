d9c54e848fb125cddd21ae848619a3436dbcc468
Jan Jorgensen <jandjorgensen@gmail.com>
2016-04-04 11:15:55 -0700

    1.0.0

---
+++ dist/cjs/can-construct-proxy.js
@@ -0,0 +1,38 @@
+/*can-construct-proxy@0.0.0#can-construct-proxy*/
+var can = require('can/util/util');
+var Construct = require('can/construct/construct');
+var isFunction = can.isFunction, isArray = can.isArray, makeArray = can.makeArray, proxy = function (funcs) {
+        var args = makeArray(arguments), self;
+        funcs = args.shift();
+        if (!isArray(funcs)) {
+            funcs = [funcs];
+        }
+        self = this;
+        return function class_cb() {
+            var cur = args.concat(makeArray(arguments)), isString, length = funcs.length, f = 0, func;
+            for (; f < length; f++) {
+                func = funcs[f];
+                if (!func) {
+                    continue;
+                }
+                isString = typeof func === 'string';
+                cur = (isString ? self[func] : func).apply(self, cur || []);
+                if (f < length - 1) {
+                    cur = !isArray(cur) || cur._use_call ? [cur] : cur;
+                }
+            }
+            return cur;
+        };
+    };
+Construct.proxy = Construct.prototype.proxy = proxy;
+var correctedClasses = [
+        can.Map,
+        can.Control,
+        can.Model
+    ], i = 0;
+for (; i < correctedClasses.length; i++) {
+    if (correctedClasses[i]) {
+        correctedClasses[i].proxy = proxy;
+    }
+}
+module.exports = Construct;
